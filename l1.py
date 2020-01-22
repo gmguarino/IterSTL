fa9322a1f3beadec4b3c085afb8efd8d980a5c42
Giuseppe Marcello Guarino <gmguarino1@gmail.com>
2020-01-22 17:40:12 +0100

    Work in Progress - skeleton in place

---
+++ l1.py
@@ -0,0 +1,221 @@
+from cvxopt import blas, lapack, solvers
+from cvxopt import matrix, spdiag, mul, div, sparse
+from cvxopt import spmatrix, sqrt, base
+
+
+def l1(P, q):
+    """
+    Returns the solution u of the ell-1 approximation problem
+
+        (primal) minimize ||P*u - q||_1
+
+        (dual)   maximize    q'*w
+                 subject to  P'*w = 0
+                             ||w||_infty <= 1.
+    """
+    P = sparse(matrix(P.todense()))
+    q = sparse(matrix(q.todense()))
+    m, n = P.size
+
+    # Solve equivalent LP
+    #
+    #     minimize    [0; 1]' * [u; v]
+    #     subject to  [P, -I; -P, -I] * [u; v] <= [q; -q]
+    #
+    #     maximize    -[q; -q]' * z
+    #     subject to  [P', -P']*z  = 0
+    #                 [-I, -I]*z + 1 = 0
+    #                 z >= 0
+
+    c = matrix(n * [0.0] + m * [1.0])
+    h = matrix([q, -q])
+
+    def Fi(x, y, alpha=1.0, beta=0.0, trans='N'):
+        if trans == 'N':
+            # y := alpha * [P, -I; -P, -I] * x + beta*y
+            u = P * x[:n]
+            y[:m] = alpha * (u - x[n:]) + beta * y[:m]
+            y[m:] = alpha * (-u - x[n:]) + beta * y[m:]
+
+        else:
+            # y := alpha * [P', -P'; -I, -I] * x + beta*y
+            y[:n] = alpha * P.T * (x[:m] - x[m:]) + beta * y[:n]
+            y[n:] = -alpha * (x[:m] + x[m:]) + beta * y[n:]
+
+    def Fkkt(W):
+
+        # Returns a function f(x, y, z) that solves
+        #
+        # [ 0  0  P'      -P'      ] [ x[:n] ]   [ bx[:n] ]
+        # [ 0  0 -I       -I       ] [ x[n:] ]   [ bx[n:] ]
+        # [ P -I -W1^2     0       ] [ z[:m] ] = [ bz[:m] ]
+        # [-P -I  0       -W2      ] [ z[m:] ]   [ bz[m:] ]
+        #
+        # On entry bx, bz are stored in x, z.
+        # On exit x, z contain the solution, with z scaled (W['di'] .* z is
+        # returned instead of z).
+
+        d1, d2 = W['d'][:m], W['d'][m:]
+        D = 4 * (d1 ** 2 + d2 ** 2) ** -1
+        A = P.T * spdiag(D) * P
+        lapack.potrf(A)
+
+        def f(x, y, z):
+            x[:n] += P.T * (mul(div(d2 ** 2 - d1 ** 2, d1 ** 2 + d2 ** 2), x[n:])
+                            + mul(.5 * D, z[:m] - z[m:]))
+            lapack.potrs(A, x)
+
+            u = P * x[:n]
+            x[n:] = div(x[n:] - div(z[:m], d1 ** 2) - div(z[m:], d2 ** 2) +
+                        mul(d1 ** -2 - d2 ** -2, u), d1 ** -2 + d2 ** -2)
+
+            z[:m] = div(u - x[n:] - z[:m], d1)
+            z[m:] = div(-u - x[n:] - z[m:], d2)
+
+        return f
+
+    # Initial primal and dual points from least-squares solution.
+
+    # uls minimizes ||P*u-q||_2; rls is the LS residual.
+    uls = +q
+    lapack.gels(+P, uls)
+    rls = P * uls[:n] - q
+
+    # x0 = [ uls;  1.1*abs(rls) ];   s0 = [q;-q] - [P,-I; -P,-I] * x0
+    x0 = matrix([uls[:n], 1.1 * abs(rls)])
+    s0 = +h
+    Fi(x0, s0, alpha=-1, beta=1)
+
+    # z0 = [ (1+w)/2; (1-w)/2 ] where w = (.9/||rls||_inf) * rls
+    # if rls is nonzero and w = 0 otherwise.
+    if max(abs(rls)) > 1e-10:
+        w = .9 / max(abs(rls)) * rls
+    else:
+        w = matrix(0.0, (m, 1))
+    z0 = matrix([.5 * (1 + w), .5 * (1 - w)])
+
+    dims = {'l': 2 * m, 'q': [], 's': []}
+    sol = solvers.conelp(c, Fi, h, dims, kktsolver=Fkkt,
+                         primalstart={'x': x0, 's': s0}, dualstart={'z': z0})
+    return sol['x'][:n]
+
+
+def l1blas(P, q):
+    """
+    Returns the solution u of the ell-1 approximation problem
+
+        (primal) minimize ||P*u - q||_1
+
+        (dual)   maximize    q'*w
+                 subject to  P'*w = 0
+                             ||w||_infty <= 1.
+    """
+
+    m, n = P.size
+
+    # Solve equivalent LP
+    #
+    #     minimize    [0; 1]' * [u; v]
+    #     subject to  [P, -I; -P, -I] * [u; v] <= [q; -q]
+    #
+    #     maximize    -[q; -q]' * z
+    #     subject to  [P', -P']*z  = 0
+    #                 [-I, -I]*z + 1 = 0
+    #                 z >= 0
+
+    c = matrix(n * [0.0] + m * [1.0])
+    h = matrix([q, -q])
+
+    u = matrix(0.0, (m, 1))
+    Ps = matrix(0.0, (m, n))
+    A = matrix(0.0, (n, n))
+
+    def Fi(x, y, alpha=1.0, beta=0.0, trans='N'):
+        if trans == 'N':
+            # y := alpha * [P, -I; -P, -I] * x + beta*y
+            blas.gemv(P, x, u)
+            y[:m] = alpha * (u - x[n:]) + beta * y[:m]
+            y[m:] = alpha * (-u - x[n:]) + beta * y[m:]
+
+        else:
+            # y := alpha * [P', -P'; -I, -I] * x + beta*y
+            blas.copy(x[:m] - x[m:], u)
+            blas.gemv(P, u, y, alpha=alpha, beta=beta, trans='T')
+            y[n:] = -alpha * (x[:m] + x[m:]) + beta * y[n:]
+
+    def Fkkt(W):
+
+        # Returns a function f(x, y, z) that solves
+        #
+        # [ 0  0  P'      -P'      ] [ x[:n] ]   [ bx[:n] ]
+        # [ 0  0 -I       -I       ] [ x[n:] ]   [ bx[n:] ]
+        # [ P -I -D1^{-1}  0       ] [ z[:m] ] = [ bz[:m] ]
+        # [-P -I  0       -D2^{-1} ] [ z[m:] ]   [ bz[m:] ]
+        #
+        # where D1 = diag(di[:m])^2, D2 = diag(di[m:])^2 and di = W['di'].
+        #
+        # On entry bx, bz are stored in x, z.
+        # On exit x, z contain the solution, with z scaled (di .* z is
+        # returned instead of z).
+
+        # Factor A = 4*P'*D*P where D = d1.*d2 ./(d1+d2) and
+        # d1 = d[:m].^2, d2 = d[m:].^2.
+
+        di = W['di']
+        d1, d2 = di[:m] ** 2, di[m:] ** 2
+        D = div(mul(d1, d2), d1 + d2)
+        Ds = spdiag(2 * sqrt(D))
+        base.gemm(Ds, P, Ps)
+        blas.syrk(Ps, A, trans='T')
+        lapack.potrf(A)
+
+        def f(x, y, z):
+            # Solve for x[:n]:
+            #
+            #    A*x[:n] = bx[:n] + P' * ( ((D1-D2)*(D1+D2)^{-1})*bx[n:]
+            #        + (2*D1*D2*(D1+D2)^{-1}) * (bz[:m] - bz[m:]) ).
+
+            blas.copy((mul(div(d1 - d2, d1 + d2), x[n:]) +
+                       mul(2 * D, z[:m] - z[m:])), u)
+            blas.gemv(P, u, x, beta=1.0, trans='T')
+            lapack.potrs(A, x)
+
+            # x[n:] := (D1+D2)^{-1} * (bx[n:] - D1*bz[:m] - D2*bz[m:]
+            #     + (D1-D2)*P*x[:n])
+
+            base.gemv(P, x, u)
+            x[n:] = div(x[n:] - mul(d1, z[:m]) - mul(d2, z[m:]) +
+                        mul(d1 - d2, u), d1 + d2)
+
+            # z[:m] := d1[:m] .* ( P*x[:n] - x[n:] - bz[:m])
+            # z[m:] := d2[m:] .* (-P*x[:n] - x[n:] - bz[m:])
+
+            z[:m] = mul(di[:m], u - x[n:] - z[:m])
+            z[m:] = mul(di[m:], -u - x[n:] - z[m:])
+
+        return f
+
+    # Initial primal and dual points from least-squares solution.
+
+    # uls minimizes ||P*u-q||_2; rls is the LS residual.
+    uls = +q
+    lapack.gels(+P, uls)
+    rls = P * uls[:n] - q
+
+    # x0 = [ uls;  1.1*abs(rls) ];   s0 = [q;-q] - [P,-I; -P,-I] * x0
+    x0 = matrix([uls[:n], 1.1 * abs(rls)])
+    s0 = +h
+    Fi(x0, s0, alpha=-1, beta=1)
+
+    # z0 = [ (1+w)/2; (1-w)/2 ] where w = (.9/||rls||_inf) * rls
+    # if rls is nonzero and w = 0 otherwise.
+    if max(abs(rls)) > 1e-10:
+        w = .9 / max(abs(rls)) * rls
+    else:
+        w = matrix(0.0, (m, 1))
+    z0 = matrix([.5 * (1 + w), .5 * (1 - w)])
+
+    dims = {'l': 2 * m, 'q': [], 's': []}
+    sol = solvers.conelp(c, Fi, h, dims, kktsolver=Fkkt,
+                         primalstart={'x': x0, 's': s0}, dualstart={'z': z0})
+    return sol['x'][:n]
---
+++ main.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from RobustSTL import bilateral_window, bilateral_filtering, estimate_period, \
+    extract_seasonality, remove_norm_mean, seasonal_difference, extract_trend
+
+np.random.seed(4)
+signal = []
+
+freq = 0.8
+print("T = ", 1 / freq)
+fs = 100
+w = 2 * np.pi * freq
+trend = 0  # 0.0025
+for i in range(500):
+    switch = np.random.rand()
+    if switch < 0.98:
+        signal.append(trend * i + np.sin(w * i / fs) + np.random.rand() * 0.05)
+    else:
+        signal.append(trend * i + np.sin(w * i / fs) + np.random.rand() * 3)
+signal = np.asarray(signal)
+plt.figure()
+plt.plot(signal)
+plt.figure()
+H = 3
+K = 2
+delta_d = 1
+delta_i = 1
+t = 146
+j = np.linspace(t - H, t + H, 2 * H + 1, dtype=int)
+window, yj = bilateral_window(signal, t, j, delta_d, delta_i)
+plt.plot(yj)
+plt.plot(window)
+
+plt.figure()
+filtered = bilateral_filtering(signal, H, delta_d, delta_i)
+plt.plot(filtered)
+
+T = estimate_period(signal, fs=fs)
+
+
+
+print(seasonal_difference(signal, T).shape, signal.size - T*fs)
+
+extract_trend(signal, T, lambda_1=1.0, lambda_2=0.5)
+plt.show()
