df7a196bc9df46400b643a9e99ba83c8944d2fd3
Giuseppe Marcello Guarino <gmguarino1@gmail.com>
2020-01-27 17:49:25 +0100

    Clean Up

--- main.py
+++ main.py
@@ -1,66 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from RobuSTL import bilateral_window, bilateral_filtering, estimate_period, \
-    extract_seasonality, adjust_season, seasonal_difference, extract_trend, get_remainder
+    extract_seasonality, adjust_season, seasonal_difference, extract_trend, RSTL, IterativeSeason, \
+    get_remainder
 
 np.random.seed(4)
 signal = []
-print("starting")
+print("Generating Signal ...")
 freq = 0.8
-print("T = ", 1 / freq)
+print("Signal period is T = ", 1 / freq)
 fs = 100
 w = 2 * np.pi * freq
-trend = 0.002
+trend = 0.003
 for i in range(500):
     switch = np.random.rand()
+    base_signal = trend * i + np.sin(w * i / fs)
     if switch < 0.98:
-        signal.append(trend * i + np.sin(w * i / fs) + np.random.rand() * 0.05)
+        signal.append(base_signal + np.random.rand() * 0.1)
     else:
-        signal.append(trend * i + np.sin(w * i / fs) + np.random.rand() * 3)
+        signal.append(base_signal + np.random.rand() * 3)
 signal = np.asarray(signal)
-plt.figure()
-plt.plot(signal)
+
 H = 3
 K = 2
 delta_d = 1
 delta_i = 1
-# t = 146
-# j = np.linspace(t - H, t + H, 2 * H + 1, dtype=int)
-# window, yj = bilateral_window(signal, t, j, delta_d, delta_i)
-# plt.plot(yj)
-# plt.plot(window)
-#
-# plt.figure()
-# filtered = bilateral_filtering(signal, H, delta_d, delta_i)
-# plt.plot(filtered)
-#
 T = estimate_period(signal, fs=fs)
-#
-# print(seasonal_difference(signal, T).shape, signal.size - T * fs)
-#
-new_signal, trend = extract_trend(signal, T, lambda_1=1.0, lambda_2=0.5)
-# plt.figure()
-# plt.plot(trend)
-# plt.plot(new_signal)
-season = extract_seasonality(new_signal, fs, H, K, delta_d, delta_i)
-plt.figure()
-plt.plot(season)
-plt.plot(new_signal)
+print(f"Calculated period is: {T} samples")
 
-filter_params = {"H": 3, "delta_d": 1, "delta_i": 1}
-season_params = {"H": 3, "K": 2, "delta_d": 1, "delta_i": 1}
+filter_params = {"H": 2, "delta_d": 0.5, "delta_i": 0.5}
 
-(remainder, filtered, season, trend) = get_remainder(signal, fs, filter_params, season_params, lambda_1=1.0,
-                                                     lambda_2=0.5)
-plt.figure()
-plt.plot(remainder, label="remainder")
-plt.plot(season, label="season")
-plt.legend()
+print("Finding Anomalies...")
+(remainder, filtered, season, trend) = RSTL(signal, fs, filter_params, decimation_rate=0.6, lambda_1=1.0, lambda_2=0.5)
 
-plt.figure()
-plt.plot(trend, label="trend")
-plt.plot(filtered, label="filtered")
+results = (signal, filtered, season, trend, remainder)
+colours = ['red', 'blue', 'green', 'black', 'violet']
 
-plt.legend()
+labels = ("signal", "filtered", "season", "trend", "remainder")
+fig, axes = plt.subplots(5, 1, sharex='col')
+for i in range(5):
+    axes[i].plot(results[i], color=colours[i])
+    axes[i].set_ylabel(labels[i])
+    axes[i].set_ylim([-1.5, 5])
+axes[-1].set_xlabel("Sample Number")
 
 plt.show()
