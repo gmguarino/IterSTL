3353184eab9dce92862a9b5e68adc0618b2baa90
Giuseppe Marcello Guarino <gmguarino1@gmail.com>
2020-01-27 18:34:16 +0100

    updated plotting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from IterSTL import estimate_period, ISTL
 
 # np.random.seed(10)
+
 signal = []
 print("Generating Signal ...")
 freq = 0.8
@@ -19,6 +20,7 @@ for i in range(500):
         signal.append(base_signal + np.random.rand() * 3)
 signal = np.asarray(signal)
 
+
 H = 3
 K = 2
 delta_d = 1
@@ -26,10 +28,10 @@ delta_i = 1
 T = estimate_period(signal, fs=fs)
 print(f"Calculated period is: {T} samples")
 
-filter_params = {"H": 2, "delta_d": 0.5, "delta_i": 0.5}
+filter_params = {"H": 5, "delta_d": 0.5, "delta_i": 0.5}
 
 print("Finding Anomalies...")
-(remainder, filtered, season, trend) = ISTL(signal, fs, filter_params, decimation_rate=0.6, lambda_1=1.0, lambda_2=0.5)
+(remainder, filtered, season, trend) = ISTL(signal, fs, filter_params, decimation_rate=0.4, lambda_1=1.0, lambda_2=0.5)
 
 results = (signal, filtered, season, trend, remainder)
 colours = ['red', 'blue', 'green', 'black', 'violet']
@@ -39,7 +41,7 @@ fig, axes = plt.subplots(5, 1, sharex='col')
 for i in range(5):
     axes[i].plot(results[i], color=colours[i])
     axes[i].set_ylabel(labels[i])
-    axes[i].set_ylim([-1.5, 5])
+    # axes[i].set_ylim([-1.5, 5])
 axes[-1].set_xlabel("Sample Number")
-
+axes[-1].plot(np.diff(results[-1]) ** 2, color='orange', alpha=0.5)
 plt.show()
